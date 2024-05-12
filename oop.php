4fea07f312f4a38c5535a54fe417f0b2f5920ea6
Anik Hassan1 <ah491606@gmail.com>
2024-05-12 20:18:21 +0600

    ColorCode

--- oop.php
+++ oop.php
@@ -52,3 +52,37 @@ $ourFund->addFund(10);
 $ourFund->deductFund(7);
 $ourFund->getTotal();
 ?>
+<?php
+class RGB{
+    private $color;
+    private $red;
+    private $green;
+    private $blue;
+
+    public function __construct($colorCode = ''){
+        $this->color=ltrim($colorCode,'#');
+        $this->parseColor();
+    }
+    public function getColor(){
+        return $this->color;
+    }
+    public function setColor($colorCode){
+          $this->color=ltrim($colorCode,"#");
+          $this->parseColor();
+    }
+    public function RGBcolor(){
+        echo "Red = {$this->red}\n Green = {$this->green}\n Blue = {$this->blue}\n";
+    }
+    private function parseColor()
+    {
+        if($this->color){
+            list($this->red,$this->green,$this->blue)=sscanf($this->color,"%02x%02x%02x");
+        }else{
+            list($this->red,$this->green,$this->blue)=array(0,0,0);
+        }
+    }
+}
+$std = new RGB('#ff0020');
+$std->RGBcolor();
+
+?>
